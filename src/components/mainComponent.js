4339b6eb4df5ffdfd95641d2ae7ba24a0fea7e50
BISWAJIT MOHANTY <biswajittlh@gmail.com>
2021-01-30 20:36:55 +0530

    initial

---
+++ src/components/mainComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function  Main() {
+    return (
+        <div>
+            <nav>
+                <a href="/" className="link1">My Bank</a>
+                <a href="/" className="link1">Home</a>
+                <a href="/" className="link2">About Us</a>
+                <a href="/" className="link3">Contact Us</a>
+            </nav>
+        </div>
+    )
+}
+
+export default  Main
